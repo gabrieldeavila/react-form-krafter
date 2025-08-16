5aefd10d4559a7f1db5da60707d8b36ae10b13ff
geavila <gabrielavila04@protonmail.com>
2025-08-16 14:26:49 -0300

    feat: optimize input handling in FieldBase component using useMemo for performance improvements

--- templates/shadcn/router-sidebar/app/components/internal/krafter/fields/base.tsx
+++ templates/shadcn/router-sidebar/app/components/internal/krafter/fields/base.tsx
@@ -1,4 +1,4 @@
-import { memo } from "react";
+import { memo, useMemo } from "react";
 import type { FieldMethods, RegisterField } from "react-form-krafter";
 import { Input } from "~/components/ui/input";
 import { Label } from "~/components/ui/label";
@@ -19,6 +19,29 @@ const FieldBase = memo(
   }: FieldBaseProps & {
     onFieldChange: (value: unknown) => string | number;
   }) => {
+    const inputProps = useMemo(() => {
+      const defaultValue =
+        field.initialValue == null
+          ? undefined
+          : type === "number"
+            ? Number(field.initialValue)
+            : String(field.initialValue);
+
+      if (field.value == null) return { defaultValue };
+
+      if (type === "number") {
+        return {
+          value: Number(field.value),
+          defaultValue: defaultValue,
+        };
+      }
+
+      return {
+        value: String(field.value),
+        defaultValue: defaultValue,
+      };
+    }, [field.value, field.initialValue, type]);
+
     return (
       <div className={cn("flex flex-col gap-2", field.wrapperClassName)}>
         <Label htmlFor={field.name}>{field.label}</Label>
@@ -30,18 +53,7 @@ const FieldBase = memo(
           name={field.name}
           required={field.required}
           disabled={field.isDisabled}
-          value={
-            type === "number"
-              ? Number(field.value)
-              : typeof field.value === "string"
-                ? field.value
-                : ""
-          }
-          defaultValue={
-            type === "number"
-              ? Number(field.initialValue)
-              : String(field.initialValue)
-          }
+          {...inputProps}
           onChange={(e) => methods.onChange(onFieldChange(e.target.value))}
           onFocus={methods.onFocus}
           onBlur={methods.onBlur}
